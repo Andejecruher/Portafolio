7a034a1077cd5150c5bb2cb6d82286e9e28a5e50
Andejecruher <andejecruher@gmail.com>
2024-11-26 18:31:25 -0600

    Agregar TypographyStylesProvider en SinglePost y ajustar estilos de código en CSS

--- src/components/Posts/SinglePost.jsx
+++ src/components/Posts/SinglePost.jsx
@@ -14,6 +14,7 @@ import {
   Stack,
   Container,
   Skeleton,
+  TypographyStylesProvider,
 } from '@mantine/core';
 import { FormComments } from '@src/components/FormComments/FormComments';
 
@@ -112,8 +113,9 @@ export function SinglePost() {
 
           {/* Article Content */}
           <Divider mb="lg" />
-          <Box className={classes.dangerousHtml} dangerouslySetInnerHTML={{ __html: content }} mb="lg" />
-
+          <TypographyStylesProvider>
+            <Box className={classes.dangerousHtml} dangerouslySetInnerHTML={{ __html: content }} mb="lg" />
+          </TypographyStylesProvider>
           {/* Comments Section */}
           <Box mt="lg">
             <Title order={2} size="h2" mb="sm" className={classes.text}>Comentarios</Title>
